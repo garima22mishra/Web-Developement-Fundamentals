c4254d28d88c1cc35a99960f3fe3e3f7a733dba6
garima.mishra <garima.mishra@cumulus-systems.com>
2023-02-12 23:39:58 +0530

    Connection pooling and security with JSP project

---
+++ ConnectionPooling/src/main/java/com/connection/pooling/ConnectionPoolingServlet.java
@@ -0,0 +1,59 @@
+package com.connection.pooling;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+/**
+ * Servlet implementation class ConnectionPoolingServlet
+ */
+@WebServlet("/ConnectionPoolingServlet")
+public class ConnectionPoolingServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public ConnectionPoolingServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			InitialContext context = new InitialContext(); 
+			// java:comp/env/ is the root context in java EE applications
+			DataSource ds = (DataSource) context.lookup("java:comp/env/myds");
+			
+			Connection con = ds.getConnection();
+			System.out.println(con);
+			
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ SecurityDemo/src/main/webapp/login.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Login Page</title>
+</head>
+<body>
+	<form action="j_security_check" method="post">
+	User Name: <input name="j_username"/><br/>
+	Password: <input type="text" name="j_password" /><br/>
+	<input type="submit" value="submit" />
+	
+	</form>
+</body>
+</html>
